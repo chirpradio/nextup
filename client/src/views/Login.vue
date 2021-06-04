e3aeac9aeafa7aadab18cdeee29aef864cbbba34
Steve Lund <lund5000@gmail.com>
2021-06-04 07:40:14 -0400

    Show loading indicator after authenticating but before data is loaded

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,32 +1,38 @@
 <template>
   <div class="col-12 col-md-6">
-    <h2>Log in to NextUp</h2>
-    <p class="text-muted" v-if="$route.query.redirect">
-      You need to log in first.
-    </p>
-    <form class="mb-3" @submit.prevent="logIn">
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input id="email" class="form-control" v-model="email" required />
-      </div>
-      <div class="mb-3">
-        <label for="password">Password</label>
-        <input
-          id="password"
-          class="form-control"
-          v-model="password"
-          type="password"
-          required
-        />
-      </div>
-      <button class="btn btn-chirp-red" type="submit">log in</button>
-    </form>
-    <p v-if="error" class="text-danger">{{ errorMessage }}</p>
+    <div v-if="!isAuthenticated">
+      <h2>Log in to NextUp</h2>
+      <p class="text-muted" v-if="$route.query.redirect">
+        You need to log in first.
+      </p>
+      <form class="mb-3" @submit.prevent="logIn">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input id="email" class="form-control" v-model="email" required />
+        </div>
+        <div class="mb-3">
+          <label for="password">Password</label>
+          <input
+            id="password"
+            class="form-control"
+            v-model="password"
+            type="password"
+            required
+          />
+        </div>
+        <button class="btn btn-chirp-red" type="submit">log in</button>
+      </form>
+      <p v-if="error" class="text-danger">{{ errorMessage }}</p>
+    </div>
+    <RecordSpinner v-if="isAuthenticated" />
   </div>
 </template>
 
 <script>
+import RecordSpinner from "../components/RecordSpinner";
+
 export default {
+  components: { RecordSpinner, },
   data() {
     return {
       email: "",
@@ -35,6 +41,11 @@ export default {
       errorMessage: "",
     };
   },
+  computed: {
+    isAuthenticated() {      
+      return this.$store.getters.isAuthenticated;    
+    },
+  },
   methods: {
     async logIn() {
       try {
@@ -42,7 +53,7 @@ export default {
         await this.$store.dispatch("logIn", {
           email: this.email,
           password: this.password,
-        });
+        });        
         await this.$store.dispatch("getCrates");
         this.$router.push(this.$route.query.redirect || "/");
       } catch (error) {
