307082918c93e79f0a45a695db54c06f8a2cf090
Steve Lund <lund5000@gmail.com>
2021-06-04 07:41:06 -0400

    Move track filters into card

--- client/src/components/music/search/track/TrackFilters.vue
+++ client/src/components/music/search/track/TrackFilters.vue
@@ -1,155 +1,159 @@
 <template>
-  <div>
-    <button class="btn btn-link-chirp-red p-0 mb-3" @click="toggleFilters">
-      {{ toggleLabel }}
-    </button>
-    <form v-if="filtersOpen" class="col-sm-8" @submit.prevent="search">
-      <div class="mb-3">
-        <label for="search">Search term</label>
-        <div class="col-sm-6">
-          <input
-            class="form-control"
-            id="search"
-            type="search"
-            v-model="query.term"
-            aria-label="search"
-          />
-        </div>
-      </div>
-      <fieldset class="row mb-4">
-        <legend>Track</legend>
-        <div class="col-12 col-md-2 mb-3">
-          <label class="">Min. length</label>
-          <select
-            id="track__duration_ms_gte_input"
-            class="form-select form-select-sm"
-            v-model="query.track.duration_ms.gte"
-          >
-            <option value="0">0:00</option>
-            <option value="30000">0:30</option>
-            <option value="60000">1:00</option>
-            <option value="90000">1:30</option>
-            <option value="120000">2:00</option>
-            <option value="150000">2:30</option>
-            <option value="180000">3:00</option>
-            <option value="210000">3:30</option>
-            <option value="240000">4:00</option>
-            <option value="270000">4:30</option>
-            <option value="300000">5:00</option>
-            <option value="360000">6:00</option>
-            <option value="420000">7:00</option>
-            <option value="480000">8:00</option>
-            <option value="540000">9:00</option>
-            <option value="600000">10:00</option>
-          </select>
-        </div>
-        <div class="col-12 col-md-2 mb-3">
-          <label>Max length</label>
-          <select
-            id="track__duration_ms_lte_input"
-            class="form-select form-select-sm"
-            v-model="query.track.duration_ms.lte"
-          >
-            <option value="">Any</option>
-            <option value="30000">0:30</option>
-            <option value="60000">1:00</option>
-            <option value="90000">1:30</option>
-            <option value="120000">2:00</option>
-            <option value="150000">2:30</option>
-            <option value="180000">3:00</option>
-            <option value="210000">3:30</option>
-            <option value="240000">4:00</option>
-            <option value="270000">4:30</option>
-            <option value="300000">5:00</option>
-            <option value="360000">6:00</option>
-            <option value="420000">7:00</option>
-            <option value="480000">8:00</option>
-            <option value="540000">9:00</option>
-            <option value="600000">10:00</option>
-          </select>
-        </div>
-        <div class="col-12 col-md-3">
-          <label class="form-check-label" for="is_recommended">
-            Recommended ⭐
-          </label>
-          <div class="form-check form-switch">
+  <div class="card">
+    <div class="card-header">
+      <button class="btn btn-link-chirp-red p-0" @click="toggleFilters">
+        {{ toggleLabel }}
+      </button>
+    </div>
+    <div v-if="filtersOpen" class="card-body">
+      <form class="col-sm-8" @submit.prevent="search">
+        <div class="mb-3">
+          <label for="search">Search term</label>
+          <div class="col-sm-6">
             <input
-              class="form-check-input"
-              type="checkbox"
-              id="is_compilation"
-              v-model="query.track.is_recommended"
+              class="form-control"
+              id="search"
+              type="search"
+              v-model="query.term"
+              aria-label="search"
             />
-            <label class="form-check-label"></label>
           </div>
         </div>
-      </fieldset>
-      <fieldset class="row mb-4">
-        <legend>Album</legend>
-        <div class="col-12 col-md-2 mb-3">
-          <label class="" for="label">Label</label>
-          <input
-            class="form-control form-control-sm"
-            id="label"
-            type="search"
-            v-model="query.album.label"
-          />
-        </div>
-        <div class="col-12 col-md-2 mb-3">
-          <label class="" for="year">Year</label>
-          <input
-            class="form-control form-control-sm"
-            id="year"
-            type="number"
-            v-model="query.album.year"
-          />
-        </div>
-        <div class="col-12 col-md-2 mb-3">
-          <label class="" for="rotation">Rotation</label>
-          <select
-            id="rotation"
-            class="form-select form-select-sm"
-            v-model="query.album.rotation"
-          >
-            <option value="any">Any</option>
-            <option value="heavy_rotation">Heavy</option>
-            <option value="light_rotation">Light</option>
-          </select>
-        </div>
-        <div class="col-12 col-md-2 mb-3">
-          <label class="" for="local">Local</label>
-          <select
-            id="local"
-            class="form-select form-select-sm"
-            v-model="query.album.local"
-          >
-            <option value="any">Any</option>
-            <option value="local_current">Current</option>
-            <option value="local_classic">Classic</option>
-          </select>
-        </div>
-        <div class="col-12 col-md-2 mb-3">
-          <label for="is_compilation">Compilation</label>
-          <div class="form-check form-switch">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              id="is_compilation"
-              v-model="query.album.is_compilation"
-            />
-            <label class="form-check-label"></label>
+        <fieldset class="row mb-4">
+          <legend>Track</legend>
+          <div class="col-12 col-md-2 mb-3">
+            <label class="">Min. length</label>
+            <select
+              id="track__duration_ms_gte_input"
+              class="form-select form-select-sm"
+              v-model="query.track.duration_ms.gte"
+            >
+              <option value="0">0:00</option>
+              <option value="30000">0:30</option>
+              <option value="60000">1:00</option>
+              <option value="90000">1:30</option>
+              <option value="120000">2:00</option>
+              <option value="150000">2:30</option>
+              <option value="180000">3:00</option>
+              <option value="210000">3:30</option>
+              <option value="240000">4:00</option>
+              <option value="270000">4:30</option>
+              <option value="300000">5:00</option>
+              <option value="360000">6:00</option>
+              <option value="420000">7:00</option>
+              <option value="480000">8:00</option>
+              <option value="540000">9:00</option>
+              <option value="600000">10:00</option>
+            </select>
           </div>
+          <div class="col-12 col-md-2 mb-3">
+            <label>Max length</label>
+            <select
+              id="track__duration_ms_lte_input"
+              class="form-select form-select-sm"
+              v-model="query.track.duration_ms.lte"
+            >
+              <option value="">Any</option>
+              <option value="30000">0:30</option>
+              <option value="60000">1:00</option>
+              <option value="90000">1:30</option>
+              <option value="120000">2:00</option>
+              <option value="150000">2:30</option>
+              <option value="180000">3:00</option>
+              <option value="210000">3:30</option>
+              <option value="240000">4:00</option>
+              <option value="270000">4:30</option>
+              <option value="300000">5:00</option>
+              <option value="360000">6:00</option>
+              <option value="420000">7:00</option>
+              <option value="480000">8:00</option>
+              <option value="540000">9:00</option>
+              <option value="600000">10:00</option>
+            </select>
+          </div>
+          <div class="col-12 col-md-3">
+            <label class="form-check-label" for="is_recommended">
+              Recommended ⭐
+            </label>
+            <div class="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                id="is_compilation"
+                v-model="query.track.is_recommended"
+              />
+              <label class="form-check-label"></label>
+            </div>
+          </div>
+        </fieldset>
+        <fieldset class="row mb-4">
+          <legend>Album</legend>
+          <div class="col-12 col-md-2 mb-3">
+            <label class="" for="label">Label</label>
+            <input
+              class="form-control form-control-sm"
+              id="label"
+              type="search"
+              v-model="query.album.label"
+            />
+          </div>
+          <div class="col-12 col-md-2 mb-3">
+            <label class="" for="year">Year</label>
+            <input
+              class="form-control form-control-sm"
+              id="year"
+              type="number"
+              v-model="query.album.year"
+            />
+          </div>
+          <div class="col-12 col-md-2 mb-3">
+            <label class="" for="rotation">Rotation</label>
+            <select
+              id="rotation"
+              class="form-select form-select-sm"
+              v-model="query.album.rotation"
+            >
+              <option value="any">Any</option>
+              <option value="heavy_rotation">Heavy</option>
+              <option value="light_rotation">Light</option>
+            </select>
+          </div>
+          <div class="col-12 col-md-2 mb-3">
+            <label class="" for="local">Local</label>
+            <select
+              id="local"
+              class="form-select form-select-sm"
+              v-model="query.album.local"
+            >
+              <option value="any">Any</option>
+              <option value="local_current">Current</option>
+              <option value="local_classic">Classic</option>
+            </select>
+          </div>
+          <div class="col-12 col-md-2 mb-3">
+            <label for="is_compilation">Compilation</label>
+            <div class="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                id="is_compilation"
+                v-model="query.album.is_compilation"
+              />
+              <label class="form-check-label"></label>
+            </div>
+          </div>
+        </fieldset>
+        <div>
+          <button class="col-4 btn btn-chirp-red" type="submit">Search</button>
+          <router-link
+            class="btn btn-link btn-link-chirp-red"
+            :to="{ path: '/library/search/track', query: { type: 'track' } }"
+          >
+            clear filters
+          </router-link>
         </div>
-      </fieldset>
-      <div>
-        <button class="col-4 btn btn-chirp-red" type="submit">Search</button>
-        <router-link
-          class="btn btn-link btn-link-chirp-red"
-          :to="{ path: '/library/search/track', query: { type: 'track' } }"
-        >
-          clear filters
-        </router-link>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -180,7 +184,7 @@ export default {
       };
     },
     toggleLabel() {
-      return this.filtersOpen ? "▾ close filters" : "▸ open filters";
+      return this.filtersOpen ? "▾ close search filters" : "▸ open search filters";
     },
   },
   created() {
