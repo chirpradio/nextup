1ac9e23db5426fddb12c61bb8fb91cb2bc9fa140
Steve Lund <lund5000@gmail.com>
2021-06-16 20:25:57 -0400

    Remove unused code

--- app/routes/reports/rotation/rotation.router.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const RotationController = require("./rotation.controller");
-
-router.get("/albums", RotationController.albumsHandler);
-router.get("/plays", RotationController.playsHandler);
-
-module.exports = router;
