368a4ca957214ba3d92f2f0b1cf5f69666fb0e4c
Steve Lund <lund5000@gmail.com>
2021-07-03 10:38:19 -0400

    Setup freeform rotation plays update as cron job

--- app/routes/tasks/tasks.router.js
+++ app/routes/tasks/tasks.router.js
@@ -4,8 +4,18 @@ const TaskController = require("./tasks.controller");
 
 router.use(express.json());
 
+function validateCronRequest(req, res, next) {
+  console.log("X-Appengine-Cron", req.get("X-Appengine-Cron"));
+  console.log("IP", req.ip);
+  next();
+}
+
 router.post("/reindex/album/:album_id", TaskController.reindexAlbumHandler);
 router.post("/reindex/tags/:index/:id", TaskController.reindexTagsHandler);
-router.post("/rotation/plays", TaskController.updateFreeformRotationPlays);
+router.get(
+  "/rotation/plays",
+  validateCronRequest,
+  TaskController.updateFreeformRotationPlays
+);
 
 module.exports = router;
