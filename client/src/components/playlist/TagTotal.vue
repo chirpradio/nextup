5eaf9a36ef7197cf591a4b887144597c3ccd5316
Steve Lund <lund5000@gmail.com>
2022-03-20 16:13:24 -0400

    Calculate categories played in last hour

---
+++ client/src/components/playlist/TagTotal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex flex-column align-items-end">
+    <Tag :tag="tag" :badge="false" class="mb-1" />
+    <span class="font-sans">{{ total }} / {{ expected }}</span>
+  </div>
+</template>
+
+<script>
+import Tag from "../music/Tag.vue";
+
+export default {
+  components: { Tag },
+  props: {
+    tag: String,
+    total: Number,
+    expected: Number,
+  },
+};
+</script>
---
+++ client/src/components/playlist/TagTotals.vue
@@ -0,0 +1,71 @@
+<template>
+  <ul class="tag-totals list-inline">
+    <li class="list-inline-item me-4">
+      <TagTotal tag="heavy_rotation" :total="heavyRotation" :expected="2" />
+    </li>
+    <li class="list-inline-item me-4">
+      <TagTotal tag="light_rotation" :total="lightRotation" :expected="3" />
+    </li>
+    <li class="list-inline-item me-4">
+      <TagTotal tag="local_classic" :total="localClassic" :expected="1" />
+    </li>
+    <li class="list-inline-item">
+      <TagTotal tag="local_current" :total="localCurrent" :expected="1" />
+    </li>
+  </ul>
+</template>
+
+<style>
+.tag-totals {
+  font-size: 0.75em;
+}
+
+@media (max-width: 576px) {
+  .tag-totals {
+    font-size: 0.6em;
+  }
+}
+</style>
+
+<script>
+import TagTotal from "../playlist/TagTotal.vue";
+
+function topOfHourIso() {
+  const date = new Date();
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return date.toISOString();
+}
+
+function countBy(events, tag) {
+  const filtered = events.filter(
+    (event) => event.categories && event.categories.includes(tag)
+  );
+  return filtered.length;
+}
+
+export default {
+  components: { TagTotal },
+  computed: {
+    events() {
+      const iso = topOfHourIso();
+      return this.$store.getters.events.filter(
+        (event) => event.established > iso
+      );
+    },
+    heavyRotation() {
+      return countBy(this.events, "heavy_rotation");
+    },
+    lightRotation() {
+      return countBy(this.events, "light_rotation");
+    },
+    localClassic() {
+      return countBy(this.events, "local_classic");
+    },
+    localCurrent() {
+      return countBy(this.events, "local_current");
+    },
+  },
+};
+</script>
