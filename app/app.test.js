2b23b192c0907b78e1950a83eaa5a6b99aa0ec36
Steve Lund <lund5000@gmail.com>
2024-03-04 19:02:09 -0500

    Add tests for API key authentication

--- app/app.test.js
+++ app/app.test.js
@@ -1,7 +1,9 @@
 const request = require("supertest");
 const app = require("./app");
 const { User } = require("./models");
+require("./services");
 jest.mock("./models");
+jest.mock("./services");
 
 describe("Authenticate with email and password", () => {
   test("Bad password returns 401", async () => {
@@ -21,5 +23,34 @@ describe("Authenticate with email and password", () => {
       .post("/api/token")
       .send({ email: "not@user.com", password: "password" });
     expect(response.statusCode).toBe(401);
+  });
+});
+
+describe("Authenticate with API key", () => {
+  test("Missing API key or JWT returns 401", async () => {
+    const response = await request(app).get("/api/crate");
+    expect(response.statusCode).toBe(401);
+  });
+
+  test("Bad API key returns 401", async () => {
+    User.findOne.mockRejectedValue(new Error("Not found"));
+    const response = await request(app).get("/api/crate?api_key=n0g00d");
+    expect(response.statusCode).toBe(401);
+  });
+
+  test("Cannot authenticate with API key as inactive user", async () => {
+    User.findOne.mockReturnValue({
+      is_active: false
+    });
+    const response = await request(app).get("/api/crate?api_key=abc123");
+    expect(response.statusCode).toBe(401);
+  });
+
+  test("Authenticates with valid API key and active user", async () => {
+    User.findOne.mockReturnValue({
+      is_active: true,
+    });    
+    const response = await request(app).get("/api/crate?api_key=abc123");
+    expect(response.statusCode).toBe(200);
   });
 });
