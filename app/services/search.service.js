fcf6c3c336eb4c3271ef1146ad117d94e101a142
Steve Lund <lund5000@gmail.com>
2024-12-25 16:12:42 -0500

    Revert "Use recommended query for search as you type"
    This reverts commit 8bf925a6e55bc99c9b9376cd968c4cea6866785f.

--- app/services/search.service.js
+++ app/services/search.service.js
@@ -143,14 +143,50 @@ function buildFuzzyArtistSearch(params, options) {
 function buildArtistSearchAsYouType(params, options) {
   return {
     query: {
-      multi_match: {
-        query: params.term,
-        type: "bool_prefix",
-        fields: [
-          "name.search_as_you_type",
-          "name.search_as_you_type._2gram",
-          "name.search_as_you_type._3gram",
-          "name.normalized_standard",
+      bool: {
+        must: {
+          match_phrase_prefix: {
+            "name.search_as_you_type": params.term,
+          },
+        },
+        // prefer matches near the beginning
+        should: [
+          {
+            span_first: {
+              match: {
+                span_multi: {
+                  match: {
+                    prefix: { name: params.term },
+                  },
+                },
+              },
+              end: 1,
+            },
+          },
+          {
+            span_first: {
+              match: {
+                span_multi: {
+                  match: {
+                    prefix: { name: params.term },
+                  },
+                },
+              },
+              end: 2,
+            },
+          },
+          {
+            span_first: {
+              match: {
+                span_multi: {
+                  match: {
+                    prefix: { name: params.term },
+                  },
+                },
+              },
+              end: 3,
+            },
+          },
         ],
       },
     },
