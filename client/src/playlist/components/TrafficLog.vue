e0a4f8dece380a54453da38862c44e4772b368a5
Steve Lund <lund5000@gmail.com>
2025-01-12 16:31:51 -0500

    Navigate between back-to-back traffic log copy

--- client/src/playlist/components/TrafficLog.vue
+++ client/src/playlist/components/TrafficLog.vue
@@ -30,49 +30,46 @@
       </button>
     </ul>
 
-    <aside id="spot" class="offcanvas offcanvas-end drawer">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasLabel">{{ spotHeading }}</h5>
+    <aside id="spot" ref="spot" class="offcanvas offcanvas-end drawer">
+      <div class="offcanvas-header d-flex align-items-center">
+        <h5 class="offcanvas-title flex-grow-1" id="offcanvasLabel">
+          {{ spotHeading(selected) }}
+        </h5>
+        <span v-if="selectedGroup" class="h5 mb-0 me-2">
+          {{ ordinal }} of {{ selectedGroup.length }} spots
+        </span>
       </div>
       <div class="offcanvas-body fs-3 fs-md-1">
-        <div class="d-flex justify-content-end">
-          <button
-            class="btn btn-light"
-            data-bs-dismiss="offcanvas"
-            aria-label="close script"
-          >
-            close script
-          </button>
-          <button
-            class="btn btn-chirp-red ms-2"
-            data-bs-dismiss="offcanvas"
-            aria-label="mark as read"
-            @click="markAsRead"
-            :disabled="hasBeenRead(selected)"
-          >
-            <font-awesome-icon icon="check" />
-            mark as read
-          </button>
-        </div>
-        <p class="mt-2">{{ selected?.spot_copy.body }}</p>
-        <div class="d-flex justify-content-end">
-          <button
-            class="btn btn-light"
-            data-bs-dismiss="offcanvas"
-            aria-label="close script"
-          >
-            close script
-          </button>
-          <button
-            class="btn btn-chirp-red ms-2"
-            data-bs-dismiss="offcanvas"
-            aria-label="mark as read"
-            @click="markAsRead"
-            :disabled="hasBeenRead(selected)"
-          >
-            <font-awesome-icon icon="check" />
-            mark as read
-          </button>
+        <TrafficLogActions
+          :previous="previousInGroup"
+          :next="nextInGroup"
+          :has-been-read="hasBeenRead(selected)"
+          @close="close"
+          @previous="previous"
+          @next="next"
+          @mark-as-read="markAsRead"
+        ></TrafficLogActions>
+        <p class="mt-2 mb-5">{{ selected?.spot_copy.body }}</p>
+        <TrafficLogActions
+          :previous="previousInGroup"
+          :next="nextInGroup"
+          :has-been-read="hasBeenRead(selected)"
+          @close="close"
+          @previous="previous"
+          @next="next"
+          @mark-as-read="markAsRead"
+        ></TrafficLogActions>
+        <div class="font-sans fs-6">
+          <div v-if="previousInGroup">
+            Previous spot: {{ spotHeading(previousInGroup) }} ({{
+              readLabel(previousInGroup)
+            }})
+          </div>
+          <div v-if="nextInGroup">
+            Next spot: {{ spotHeading(nextInGroup) }} ({{
+              readLabel(nextInGroup)
+            }})
+          </div>
         </div>
       </div>
     </aside>
@@ -84,31 +81,56 @@ import { Offcanvas } from "bootstrap"; // eslint-disable-line no-unused-vars
 import { mapStores } from "pinia";
 import { useSpotsStore } from "@/traffic-log/store";
 import LoadingButton from "@/components/LoadingButton.vue";
+import TrafficLogActions from "./TrafficLogActions.vue";
 
 export default {
-  components: { LoadingButton },
+  components: { LoadingButton, TrafficLogActions },
   data() {
     return {
+      drawer: undefined,
       selected: null,
     };
   },
   computed: {
     ...mapStores(useSpotsStore),
-    spotHeading() {
-      return this.selected
-        ? `${this.time(this.selected)} ${this.selected.spot.title}`
-        : "";
-    },
     loading() {
       return this.spotsStore.loadingTrafficLog;
     },
     trafficLog() {
-      const log = this.spotsStore.trafficLog;
-      return log ? log.filter((entry) => entry.spot) : [];
+      return this.spotsStore.trafficLog;
+    },
+    selectedGroup() {
+      return this.spotsStore.group(this.selected);
+    },
+    selectedIndexInGroup() {
+      return this.selectedGroup?.indexOf(this.selected);
+    },
+    previousInGroup() {
+      return this.selectedGroup
+        ? this.selectedGroup[this.selectedIndexInGroup - 1]
+        : undefined;
+    },
+    nextInGroup() {
+      return this.selectedGroup
+        ? this.selectedGroup[this.selectedIndexInGroup + 1]
+        : undefined;
+    },
+    ordinal() {
+      switch (this.selectedIndexInGroup) {
+        case 0:
+          return "1st";
+        case 1:
+          return "2nd";
+        case 2:
+          return "3rd";
+        default:
+          return `${this.selectedIndexInGroup}th`;
+      }
     },
   },
   mounted() {
     this.spotsStore.getTrafficLog();
+    this.drawer = new Offcanvas(this.$refs.spot);
   },
   methods: {
     time(entry) {
@@ -116,17 +138,41 @@ export default {
       const minute = entry.slot.toString().padStart(2, "0");
       return `${hour}:${minute}`;
     },
+    spotHeading(entry) {
+      if (!entry) {
+        return "";
+      }
+
+      return `${this.time(entry)} ${entry.spot.title}`;
+    },
     refresh() {
       this.spotsStore.getTrafficLog();
     },
     select(entry) {
       this.selected = entry;
     },
+    close() {
+      this.drawer.hide();
+    },
+    previous() {
+      this.selected = this.previousInGroup;
+    },
+    next() {
+      this.selected = this.nextInGroup;
+    },
     async markAsRead() {
       await this.spotsStore.addTrafficLogEntry(this.selected);
+      if (this.nextInGroup) {
+        this.next();
+      } else {
+        this.close();
+      }
     },
     hasBeenRead(entry) {
       return entry && entry.readtime;
+    },
+    readLabel(entry) {
+      return this.hasBeenRead(entry) ? "read" : "unread";
     },
     titleClass(entry) {
       return {
