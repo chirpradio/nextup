61e35105357485385b6f9e8a0390b5c48f84f929
Steve Lund <lund5000@gmail.com>
2025-01-18 11:43:38 -0500

    Continue supporting type as an alias for index

--- app/routes/api/search/controller.js
+++ app/routes/api/search/controller.js
@@ -5,7 +5,8 @@ module.exports = {
     try {
       const from = req.query.offset || 0;
       const size = req.query.limit || 50;
-      const results = await SearchService.search(req.query, req.query.index, {
+      const index = req.query.index || req.query.type;
+      const results = await SearchService.search(req.query, index, {
         from,
         size,
         as_you_type: req.query.as_you_type,
