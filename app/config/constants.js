8facab294bf7d96a568de964a2be382eaec18ac0
Steve Lund <lund5000@gmail.com>
2025-02-24 18:30:53 -0500

    Move local and rotation tags to constants file

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,3 +1,9 @@
 module.exports = {
+  CURRENT_TAGS: [
+    "local_current",
+    "local_classic",
+    "heavy_rotation",
+    "light_rotation",
+  ],
   SPOT__VALID_SLOTS: [0, 12, 13, 24, 25, 30, 36, 37, 48, 49, 54, 55],
 };
