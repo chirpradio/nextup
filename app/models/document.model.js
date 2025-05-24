b54d1c2e9e0f85f3fc491d43a3c7d33dbf38d266
Steve Lund <lund5000@gmail.com>
2025-05-24 16:37:06 -0400

    Update defaults and validation

--- app/models/document.model.js
+++ app/models/document.model.js
@@ -2,20 +2,21 @@ const gstore = require("../db").gstore;
 const { Schema } = gstore;
 
 const documentSchema = new Schema({
-  author: { type: Schema.Types.Key, ref: "User", required: true },
+  author: { type: Schema.Types.Key, ref: "User" },
+  // if the author is not a user
   author_name: { type: String },
-  created: { type: Date, required: true },
-  doctype: { type: String, required: true },
+  created: { type: Date, required: true, default: gstore.defaultValues.NOW },
+  doctype: { type: String, required: true, values: ["review", "comment"] },
   is_hidden: { type: Boolean, required: true, default: false },
-  modified: { type: Date, required: true },
+  modified: { type: Date, required: true, default: gstore.defaultValues.NOW },
   revoked: { type: Boolean, required: true, default: false },
   subject: { type: Schema.Types.Key, required: true },
-  unsafe_text: { type: String },
+  unsafe_text: { type: String, required: true },
 });
 
 /**
   properties intentionally left out:
-  - timestamp: seems to always match created value
+  - timestamp: early records always match created value, missing from latest entities
   - title: null for all records
 **/
 
