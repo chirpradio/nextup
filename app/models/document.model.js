9ac2f990473289789106a277a3063f64ef1a4041
Steve Lund <lund5000@gmail.com>
2025-05-25 17:25:23 -0400

    Add first document API endpoint

--- app/models/document.model.js
+++ app/models/document.model.js
@@ -1,11 +1,16 @@
-const gstore = require("../db").gstore;
+const { gstore, datastore } = require("../db");
 const { Schema } = gstore;
 
 const documentSchema = new Schema({
   author: { type: Schema.Types.Key, ref: "User" },
   // if the author is not a user
   author_name: { type: String },
-  created: { type: Date, required: true, default: gstore.defaultValues.NOW },
+  created: {
+    type: Date,
+    required: true,
+    default: gstore.defaultValues.NOW,
+    write: false,
+  },
   doctype: { type: String, required: true, values: ["review", "comment"] },
   is_hidden: { type: Boolean, required: true, default: false },
   modified: { type: Date, required: true, default: gstore.defaultValues.NOW },
@@ -19,5 +24,23 @@ const documentSchema = new Schema({
   - timestamp: early records always match created value, missing from latest entities
   - title: null for all records
 **/
+
+async function hasAuthorOrName() {
+  const hasAuthor = datastore.isKey(this.author);
+  const hasName = typeof this.author_name === "string";
+
+  if (hasAuthor || hasName) {
+    return Promise.resolve();
+  }
+
+  return Promise.reject("Requires author or author_name");
+}
+documentSchema.pre("save", hasAuthorOrName);
+
+async function updateModified() {
+  this.modified = new Date();
+  return Promise.resolve();
+}
+documentSchema.pre("save", updateModified);
 
 module.exports = gstore.model("Document", documentSchema);
