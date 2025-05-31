da7e86d0b6ab463ff685cd6e89dff042257ed061
Steve Lund <lund5000@gmail.com>
2025-05-31 17:15:04 -0400

    Show backdrop and allow scroll based on width
    - Hide the backdrop and allow scrolling in larger viewports
    - Show the backdrop and prevent scrolling on smaller viewports
    - Adjust gracefully when the browser is resized

--- client/src/playlist/components/TrafficLog.vue
+++ client/src/playlist/components/TrafficLog.vue
@@ -75,6 +75,9 @@ import { mapStores } from "pinia";
 import { useTrafficLogStore } from "../trafficLogStore";
 import TrafficLogActions from "./TrafficLogActions.vue";
 import RecordSpinner from "@/components/RecordSpinner.vue";
+import { debounce } from "lodash";
+
+let debouncedResizeHandler;
 
 export default {
   components: { TrafficLogActions, RecordSpinner },
@@ -127,9 +130,34 @@ export default {
     this.trafficLogStore.pollForEntries();
   },
   mounted() {
-    this.drawer = new Offcanvas(this.$refs.spot);
+    debouncedResizeHandler = debounce(this.onResize, 150);
+    window.addEventListener("resize", debouncedResizeHandler);
+    this.createOffcanvas();
+  },
+  beforeUnmount() {
+    this.drawer.hide();
+  },
+  unmounted() {
+    window.removeEventListener("resize", debouncedResizeHandler);
   },
   methods: {
+    onResize() {
+      const shown = this.drawer._isShown;
+      if (shown) {
+        this.drawer.hide();
+      }
+      this.createOffcanvas();
+      if (shown) {
+        this.drawer.show();
+      }
+    },
+    createOffcanvas() {
+      const inLargeViewport = document.documentElement.clientWidth >= 1200;
+      this.drawer = new Offcanvas(this.$refs.spot, {
+        backdrop: !inLargeViewport,
+        scroll: inLargeViewport,
+      });
+    },
     time(entry) {
       let hour;
       if (entry.hour > 12) {
