501a8057fdf5b63c93979cfe96a4c30b5718a1ed
Steve Lund <lund5000@gmail.com>
2025-06-02 19:50:55 -0400

    Move feature flags into separate file
    Load feature flags by mode: https://vite.dev/guide/env-and-mode

--- client/src/stores/auth.js
+++ client/src/stores/auth.js
@@ -2,41 +2,13 @@ import { defineStore } from "pinia";
 import api from "../services/api.service";
 import jwt_decode from "jwt-decode";
 import Cookies from "js-cookie";
+import features from "./features";
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
     token: "",
     user: {},
-    features: {
-      /**
-       * template: {
-       *   users: [],
-       *   roles: [],
-       **/
-      playlist: {
-        users: [
-          "tony@chirpradio.org",
-          "jennamlchapman@gmail.com",
-          "alexarchibeque0024@gmail.com",
-          "buesary@gmail.com",
-          "chris.r.siuty@gmail.com",
-          "cornelia.q.smith@gmail.com",
-          "craigb@chirpradio.org",
-          "griffimichael@gmail.com",
-          "jenn@chirpradio.org",
-          "kevinshields101@hotmail.com",
-          "mike@nikolich.com",
-          "namowhammo@yahoo.com",
-          "scott@chirpradio.org",
-          "studio@joslinlakedesign.com",
-          "mattbarr1516@yahoo.com",
-          "pigface13@yahoo.com",
-        ],
-      },
-      "traffic-log": {
-        roles: ["traffic_log_admin"],
-      },
-    },
+    features: features[import.meta.env.MODE],
   }),
   getters: {
     isAuthenticated: (state) => {
---
+++ client/src/stores/features.js
@@ -0,0 +1,46 @@
+/**
+ * Feature flags by environment and by user and/or role.
+ *
+ * environment: {
+ *  feature: {
+ *    users: [],
+ *    roles: [],
+ *  }
+ * }
+ **/
+
+export default {
+  development: {
+    playlist: {
+      roles: ["dj"],
+    },
+    "traffic-log": {
+      roles: ["traffic_log_admin"],
+    },
+  },
+  production: {
+    playlist: {
+      users: [
+        "tony@chirpradio.org",
+        "jennamlchapman@gmail.com",
+        "alexarchibeque0024@gmail.com",
+        "buesary@gmail.com",
+        "chris.r.siuty@gmail.com",
+        "cornelia.q.smith@gmail.com",
+        "craigb@chirpradio.org",
+        "griffimichael@gmail.com",
+        "jenn@chirpradio.org",
+        "kevinshields101@hotmail.com",
+        "mike@nikolich.com",
+        "namowhammo@yahoo.com",
+        "scott@chirpradio.org",
+        "studio@joslinlakedesign.com",
+        "mattbarr1516@yahoo.com",
+        "pigface13@yahoo.com",
+      ],
+    },
+    "traffic-log": {
+      roles: ["traffic_log_admin"],
+    },
+  },
+};
