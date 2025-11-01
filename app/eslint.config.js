400a50d67ea43080283165a263acf12177c60690
Steve Lund <lund5000@gmail.com>
2025-11-01 13:45:59 -0400

    Update app lint packages to latest

---
+++ app/eslint.config.js
@@ -0,0 +1,66 @@
+const js = require('@eslint/js');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.js'],
+  },
+
+  {
+    name: 'app/files-to-ignore', 
+    ignores: ['client/**', 'node_modules/**'],
+  },
+
+  js.configs.recommended,
+
+  {
+    name: 'app/node-config',
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'commonjs',
+      globals: {
+        // Node.js globals
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        setImmediate: 'readonly',
+        clearImmediate: 'readonly',
+        // Jest globals
+        describe: 'readonly',
+        test: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        jest: 'readonly',
+      },
+    },
+  },
+
+  prettierConfig,
+
+  {
+    name: 'app/custom-rules',
+    rules: {
+      'no-unused-vars': [
+        'error',
+        {
+          caughtErrors: 'none',
+        },
+      ],
+    },
+  },
+];
