d409eef9f665a505b05862d8a042e73b64ab573e
Steve Lund <lund5000@gmail.com>
2025-11-02 17:04:01 -0500

    Playlist report (#227)
    * Add playlist report endpoint
    
    * Add /playlist/report route
    
    * Check for album
    
    * Default to yesterday as start and end date
    
    * Convert to Chicago time
    
    * Work within maximum of 1000 keys per call
    
    * Optimize album retrieval
    
    * Add local flag
    
    * Add date range validation
    
    - Require both dates to enable download button
    - Require start date before end date
    - Max 100 day span, mostly to prevent accidentally getting a year of data or more
    
    * Move max date range into constants

--- app/routes/api/playlist/validators.js
+++ app/routes/api/playlist/validators.js
@@ -9,6 +9,27 @@ function toKey(value) {
 
 const validateEnd = query("end").optional().isInt().toInt();
 const validateStart = query("start").optional().isInt().toInt();
+const validateReportStart = query("start").isISO8601();
+const validateReportEnd = query("end").isISO8601();
+const validateDateRange = function (req, res, next) {
+  try {
+    const startDate = new Date(req.query.start);
+    const endDate = new Date(req.query.end);
+    const diffInMs = endDate - startDate;
+    const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+    const MAX_DATE_RANGE_IN_DAYS = 100;
+    
+    if (diffInDays > MAX_DATE_RANGE_IN_DAYS) {
+      return res.status(400).json({
+        error: `Date range cannot exceed ${MAX_DATE_RANGE_IN_DAYS} days`
+      });
+    }
+    
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 const validateAlbum = body("album").isArray().customSanitizer(toKey);
 const validateArtist = body("artist").isArray().customSanitizer(toKey);
 const validateCategories = body("categories").isArray();
@@ -25,15 +46,30 @@ const validateRole = function (req, res, next) {
     next(error);
   }
 };
+const validateMusicDirectorRole = function (req, res, next) {
+  try {
+    if (req.user.isMusicDirector()) {
+      next();
+    } else {
+      throw new Error(errorMessages.FORBIDDEN);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 const validateTrack = body("track").isArray().customSanitizer(toKey);
 
 module.exports = {
   validateAlbum,
   validateArtist,
   validateCategories,
+  validateDateRange,
   validateEnd,
   validateFreeformTrackTitle,
   validateLabel,
+  validateMusicDirectorRole,
+  validateReportEnd,
+  validateReportStart,
   validateRole,
   validateStart,
   validateTrack,
